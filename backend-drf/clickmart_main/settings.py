b1a0f388c2d4b0a1117cb408f2ede05c3bd114eb
SriRam-008 <sriramcse1205@gmail.com>
2026-01-04 02:34:35 +0530

    added csrf trustef origins

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -187,7 +187,7 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = ['http://localhost:5173','http://172.235.17.169:5173','http://djangoclickmart.duckdns.org','https://djangoclickmart.duckdns.org','http://www.djangoclickmart.duckdns.org','https://www.djangoclickmart.duckdns.org']
 
 
-CORS_ALLOWED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "https://djangoclickmart.duckdns.org",
     "https://www.djangoclickmart.duckdns.org",
